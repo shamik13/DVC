4cc73298b8047fb6b91bd95856ea05a129025d26
TaikiInoue <taikiinoue45@gmail.com>
2020-08-14 20:48:20 +0900

    Change data structure

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,11 @@
 # for make debug
-cp /tmp/.ssh /root/.ssh -r
-cp /tmp/.zshrc /root/.zshrc
-cp /tmp/.tmux.conf /root/.tmux.conf
-sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
-/usr/bin/zsh
+# cp /tmp/.ssh /root/.ssh -r
+# cp /tmp/.zshrc /root/.zshrc
+# cp /tmp/.tmux.conf /root/.tmux.conf
+# sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
+# /usr/bin/zsh
 
 # for make run
-# dvc pull zip_dvc/202008131442_H.zip.dvc
-# dvc repro
+dvc pull zip_dvc/202008131442_H.zip.dvc
+cd repro
+dvc repro
